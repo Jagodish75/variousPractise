ad97a0b88a00d1cca76d7872d98111d893db7a1f
Jagodish75 <joyprogramming75@gmail.com>
2025-01-24 05:01:30 +0600

    getter setter

--- w3object/five.js
+++ w3object/five.js
@@ -1,7 +1,43 @@
+// const person = {
+//     firstName: "John",
+//     lastName: "Doe",
+//     language: "Bangla",
+//     set lang(value) {
+//         this.language = value;
+//     },
+// };
+// person.lang = "English";
+// console.log(person.language);
+
+// const person = {
+//     firstName: "John",
+//     lastName: "Doe",
+//     fullName: function () {
+//         return this.firstName + " " + this.lastName;
+//     },
+// };
+// console.log(person.fullName());
+
+// const person2 = {
+//     firstName: "Jagodish",
+//     lastName: "Roy",
+//     get fullName() {
+//         return this.firstName + " " + this.lastName;
+//     },
+// };
+// console.log(person2.fullName);
+
+// Create an object:
 const person = {
-    firstName: "John",
-    lastName: "Doe",
-    language: "EN",
+    firstName: "Jagodish",
+    lastName: "Roy",
+    Language: "English",
+    // get lang() {
+    //     return this.Language.toUpperCase();
+    // },
+    set lang(lang) {
+        return (this.Language = lang.toUpperCase());
+    },
 };
-const result = Object.getOwnPropertyNames(person);
-console.log(result);
+person.lang = "Bangla";
+console.log(person.Language);
