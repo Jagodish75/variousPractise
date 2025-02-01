f613079015050088cc554c392408365be5c03546
Jagodish75 <joyprogramming75@gmail.com>
2025-02-02 00:22:09 +0600

    with another more practise object

--- w3object/seven.js
+++ w3object/seven.js
@@ -1,4 +1,4 @@
-// const person = {
+//  / const person = {
 //     firstName: "jagodish",
 //     lastName: "Roy",
 // };
@@ -9,14 +9,45 @@
 // person2.house = "Rajpur";
 // console.log(person2);
 
-function Person(firstName, lastName, age, dob) {
-    this.firstName = firstName;
-    this.lastName = lastName;
-    this.age = age;
-    this.dob = dob;
+// const person1 = {
+//     firstName: "John",
+//     lastName: "Doe",
+//     age: 50,
+//     eyeColor: "blue",
+// };
+// const person2 = {
+//     firstName: "Jagodish",
+//     lastName: "Roy",
+// };
+// const person3 = Object.assign(person1, person2);
+// console.log(person3);
+
+// const person = {
+//     firstName: "John",
+//     lastName: "Doe",
+//     age: 50,
+//     eyeColor: "blue",
+// };
+// for (const [key, value] of Object.keys(person)) {
+// console.log(`${key} : ${value}`);
+// }
+// const fruits = [
+//     ["apples", 300],
+//     ["pears", 900],
+//     ["bananas", 500],
+// ];
+// console.log(Object.fromEntries(fruits));
+// console.log(Object.keys(person));
+// console.log(Object.values(person));
+
+// / Create an Array
+const fruits = [
+    { name: "apples", quantity: 300 },
+    { name: "bananas", quantity: 500 },
+    { name: "oranges", quantity: 200 },
+    { name: "kiwi", quantity: 150 },
+];
+// Callback function to select low volumes
+function myCallback({ quantity }) {
+    return quantity > 200 ? "ok" : "low";
 }
-Person.nationality = "Bangladeshi";
-Person.prototype.nationality = "Bangladeshi";
-const myFather = new Person("Anukul", "Barman", 65, 1956);
-// console.log(myFather);
-console.log(myFather);
